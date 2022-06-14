ea773bbd1ce5db91916aa02c3edd7f70f947aeae
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2022-06-14 18:48:19 +0800

    sync

--- sync.sh
+++ sync.sh
@@ -6,10 +6,10 @@ cp /tmp/data_sync/sqlite.db /dashboard/data/sqlite.db
 cd /tmp/data_sync
 while true
 do
+    sleep 60
     DATE=`date`
     cp /dashboard/data/sqlite.db /tmp/data_sync/
     git add ./sqlite.db
     git commit -am "save in $DATE" --amend
     git push -f
-    sleep 60
 done
