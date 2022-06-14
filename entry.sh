4adc98a42b38642c88e05144841edbbc6dfaf118
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2022-06-14 19:03:03 +0800

    sync

--- entry.sh
+++ entry.sh
@@ -6,6 +6,12 @@ sed -i "s/nz_admin_logins/$OAUTH_ADMINS/g" /dashboard/data/config.yaml
 sed -i "s/nz_github_oauth_client_id/$OAUTH_CLIENT_ID/g" /dashboard/data/config.yaml
 sed -i "s/nz_github_oauth_client_secret/$OAUTH_CLIENT_SECRET/g" /dashboard/data/config.yaml
 sed -i "s/nz_site_title/$SITE_TITLE/g" /dashboard/data/config.yaml
+
+git config --global user.email "nezha@github.com"
+git config --global user.name "NezhaPanel"
+git clone $DATA_REPO /tmp/data_sync
+cp /tmp/data_sync/sqlite.db /dashboard/data/sqlite.db
+
 chisel server --port 9992 &
 /bin/sh /dashboard/sync.sh &
 nginx
--- sync.sh
+++ sync.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-git config --global user.email "nezha@github.com"
-git config --global user.name "NezhaPanel"
-git clone $DATA_REPO /tmp/data_sync
-cp /tmp/data_sync/sqlite.db /dashboard/data/sqlite.db
 cd /tmp/data_sync
 while true
 do
