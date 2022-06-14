d2496e82c4a8559fd2c51db95404de3d52ccd1df
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2022-06-14 16:44:24 +0800

    chisel as fronend

--- entry.sh
+++ entry.sh
@@ -6,6 +6,6 @@ sed -i "s/nz_admin_logins/$OAUTH_ADMINS/g" /dashboard/data/config.yaml
 sed -i "s/nz_github_oauth_client_id/$OAUTH_CLIENT_ID/g" /dashboard/data/config.yaml
 sed -i "s/nz_github_oauth_client_secret/$OAUTH_CLIENT_SECRET/g" /dashboard/data/config.yaml
 sed -i "s/nz_site_title/$SITE_TITLE/g" /dashboard/data/config.yaml
-chisel server --port 9992 &
+chisel server --port $PORT --backend http://localhost:9990 &
 nginx
 /dashboard/app
