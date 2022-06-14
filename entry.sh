8a1c78321670b17a81c40e6a9203f7d7ffce6957
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2022-06-14 16:39:56 +0800

    typo

--- entry.sh
+++ entry.sh
@@ -3,7 +3,7 @@ sed -i "s/nz_http_port/$PORT/g" /etc/nginx/http.d/default.conf
 
 sed -i "s/nz_oauth_type/$OAUTH_TYPE/g" /dashboard/data/config.yaml
 sed -i "s/nz_admin_logins/$OAUTH_ADMINS/g" /dashboard/data/config.yaml
-sed -i "s/nz_github_oauth_client_ide/$OAUTH_CLIENT_ID/g" /dashboard/data/config.yaml
+sed -i "s/nz_github_oauth_client_id/$OAUTH_CLIENT_ID/g" /dashboard/data/config.yaml
 sed -i "s/nz_github_oauth_client_secret/$OAUTH_CLIENT_SECRET/g" /dashboard/data/config.yaml
 sed -i "s/nz_site_title/$SITE_TITLE/g" /dashboard/data/config.yaml
 chisel server --port 9992 &
